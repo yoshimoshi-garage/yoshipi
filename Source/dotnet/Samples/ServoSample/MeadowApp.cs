d88e179649f2f2b4a88da400cd5297bccceabd30
Chris Tacke <ctacke@opennetcf.com>
2024-08-06 17:17:05 -0500

    added joystick to servo sample

--- Source/dotnet/Samples/ServoSample/MeadowApp.cs
+++ Source/dotnet/Samples/ServoSample/MeadowApp.cs
@@ -1,100 +1,227 @@
 ﻿using Meadow;
-using Meadow.Foundation.ICs.IOExpanders;
-using Meadow.Foundation.Servos;
+using Meadow.Foundation.Graphics.MicroLayout;
 using Meadow.Hardware;
-using Meadow.Peripherals;
-using Meadow.Units;
+using Meadow.Peripherals.Displays;
 using YoshiPi;
+using static Meadow.Resolver;
 
 namespace ServoSample;
 
+public class DisplayService
+{
+    private IPixelDisplay _display;
+    private DisplayScreen _screen;
+    private Box _upBox;
+    private Box _downBox;
+    private Box _leftBox;
+    private Box _rightBox;
+    private Box _centerBox;
+
+    public DisplayService(IPixelDisplay display)
+    {
+        _display = display;
+        _screen = new DisplayScreen(_display, RotationType._270Degrees)
+        {
+            BackgroundColor = Color.Black
+        };
+    }
+
+    public async Task Start()
+    {
+        CreateLayouts();
+    }
+
+    private void CreateLayouts()
+    {
+        var uiWidth = 50;
+
+        var y = (_screen.Height - uiWidth) / 2;
+
+        _centerBox = new Box(
+            (_screen.Width - uiWidth) / 2,
+            (_screen.Height - uiWidth) / 2,
+            uiWidth,
+            uiWidth)
+        {
+            IsFilled = false,
+            ForeColor = Color.Cyan
+        };
+
+        _upBox = new Box(
+            (_screen.Width - uiWidth) / 2,
+            (int)(_centerBox.Top - uiWidth * 1.5),
+            uiWidth,
+            (int)(uiWidth * 1.5))
+        {
+            IsFilled = false,
+            ForeColor = Color.Cyan
+        };
+
+        _downBox = new Box(
+            (_screen.Width - uiWidth) / 2,
+            _centerBox.Bottom,
+            uiWidth,
+            (int)(uiWidth * 1.5))
+        {
+            IsFilled = false,
+            ForeColor = Color.Cyan
+        };
+
+        _leftBox = new Box(
+            (int)(_centerBox.Left - uiWidth * 1.5),
+            _centerBox.Top,
+            (int)(uiWidth * 1.5),
+            uiWidth)
+        {
+            IsFilled = false,
+            ForeColor = Color.Cyan
+        };
+
+        _rightBox = new Box(
+            _centerBox.Right,
+            _centerBox.Top,
+            (int)(uiWidth * 1.5),
+            uiWidth)
+        {
+            IsFilled = true,
+            ForeColor = Color.Cyan
+        };
+
+        _screen.Controls.Add(_centerBox, _upBox, _downBox, _leftBox, _rightBox);
+    }
+
+    public void SetButtonState(bool isDown)
+    {
+        _centerBox.IsFilled = isDown;
+    }
+
+    public void SetXValue(float xValue)
+    {
+        _screen.BeginUpdate();
+
+        var mid = 3.3 / 2d;
+        var midl = mid - 0.1;
+        var midh = mid + 0.1;
+
+        _leftBox.IsFilled = false;
+        _rightBox.IsFilled = false;
+
+        if (xValue > midh)
+        { // right
+            var magnitude = (float)((xValue - mid) / (2 * mid));
+            var color = Color.FromHsba(180, 1, magnitude);
+            _rightBox.ForeColor = color;
+            _rightBox.IsFilled = true;
+        }
+        else if (xValue < midl)
+        { // left
+            var magnitude = (float)((mid - xValue) / (2 * mid));
+            var color = Color.FromHsba(180, 1, magnitude);
+            _leftBox.ForeColor = color;
+            _leftBox.IsFilled = true;
+        }
+        else
+        {
+            _leftBox.ForeColor = Color.Cyan;
+            _rightBox.ForeColor = Color.Cyan;
+        }
+
+        _screen.EndUpdate();
+    }
+
+    public void SetYValue(float yValue)
+    {
+        _screen.BeginUpdate();
+
+        var mid = 3.3 / 2d;
+        var midl = mid - 0.1;
+        var midh = mid + 0.1;
+
+        _upBox.IsFilled = false;
+        _downBox.IsFilled = false;
+
+        if (yValue > midh)
+        { // up
+            var magnitude = (float)((yValue - mid) / (2 * mid));
+            var color = Color.FromHsba(180, 1, magnitude);
+            _upBox.ForeColor = color;
+            _upBox.IsFilled = true;
+        }
+        else if (yValue < midl)
+        { // down
+            var magnitude = (float)((mid - yValue) / (2 * mid));
+            var color = Color.FromHsba(180, 1, magnitude);
+            _downBox.ForeColor = color;
+            _downBox.IsFilled = true;
+        }
+        else
+        {
+            _upBox.ForeColor = Color.Cyan;
+            _downBox.ForeColor = Color.Cyan;
+        }
+
+        _screen.EndUpdate();
+    }
+}
+
+public class InputService
+{
+    public event EventHandler<bool>? ButtonStateChanged;
+    public event EventHandler<float>? YValueChanged;
+    public event EventHandler<float>? XValueChanged;
+
+    public InputService(IDigitalInterruptPort buttonPort, IAnalogInputPort xInput, IAnalogInputPort yInput)
+    {
+        buttonPort.Changed += OnButtonChanged;
+        xInput.Updated += OnXInputUpdated;
+        yInput.Updated += OnYInputUpdated;
+
+        xInput.StartUpdating(TimeSpan.FromMilliseconds(200));
+        yInput.StartUpdating(TimeSpan.FromMilliseconds(200));
+    }
+
+    private void OnXInputUpdated(object? sender, IChangeResult<Meadow.Units.Voltage> e)
+    {
+        XValueChanged?.Invoke(this, (float)e.New.Volts);
+    }
+
+    private void OnYInputUpdated(object? sender, IChangeResult<Meadow.Units.Voltage> e)
+    {
+        YValueChanged?.Invoke(this, (float)e.New.Volts);
+    }
+
+    private void OnButtonChanged(object? sender, DigitalPortResult e)
+    {
+        Log.Info($"Button is {e.New.State}");
+
+        ButtonStateChanged?.Invoke(this, e.New.State);
+    }
+}
+
 public class MeadowApp : YoshiPiApp
 {
-    private ContinuousRotationServo? _crServo;
-    private AngularServo? _aServo;
-
-    //private Servo? _servo;
-    private Pca9685 _pca9685;
-    private IPwmPort _pwm;
-
-    private double _speed = 0;
+    private DisplayService _displayService;
+    private InputService _inputService;
 
     public override Task Initialize()
     {
-        Resolver.Log.Info("Initialize...");
+        _displayService = new DisplayService(Hardware.Display);
+        _inputService = new InputService(
+            Hardware.Gpio.Pins.D00.CreateDigitalInterruptPort(
+                InterruptMode.EdgeBoth, ResistorMode.InternalPullUp),
+            Hardware.Adc.Pins.A01.CreateAnalogInputPort(1),
+            Hardware.Adc.Pins.A00.CreateAnalogInputPort());
 
-        _pca9685 = new Pca9685(Hardware.GroveI2c);
-        _pwm = _pca9685.CreatePwmPort(_pca9685.Pins.LED0, 0.5f, false);
+        _inputService.ButtonStateChanged += (s, e) => _displayService.SetButtonState(!e);
+        _inputService.XValueChanged += (s, e) => _displayService.SetXValue(e);
+        _inputService.YValueChanged += (s, e) => _displayService.SetYValue(e);
 
-        //CreateContinuousRotationServo();
-        CreateAngularServo();
-
-        //_servo = new Servo(_pwm, NamedServoConfigs.SG90);
-
-
-        //_servo = new ContinuousRotationServo(_pwm, config)
-        //{
-        //    TrimDuration = TimeSpan.FromMicroseconds(100)
-        //};
-        //_servo?.Rotate(RotationDirection.None, 0);
-
-        //_servo = new Servo(_pwm, NamedServoConfigs.SG90);
-        //_servo.RotateTo(new Angle(0)).Wait();
-
-        Hardware.Button1.PressStarted += CcwButtonPressStarted;
-        Hardware.Button1.PressEnded += ButtonPressEnded;
-        Hardware.Button2.PressStarted += CwButtonPressStarted;
-        Hardware.Button2.PressEnded += ButtonPressEnded;
-
-        return Task.CompletedTask;
+        return base.Initialize();
     }
 
-    private void CreateAngularServo()
+    public override async Task Run()
     {
-        _aServo = new Mg996(_pwm)
-        {
-            //            TrimOffset = TimeSpan.FromMicroseconds(-140)
-        };
-    }
-
-    private void CreateContinuousRotationServo()
-    {
-        _crServo = new Fs90r(_pwm)
-        {
-            TrimOffset = TimePeriod.FromMicroseconds(100)
-        };
-    }
-
-    private double pwmCalibration = 100d;
-
-    private async void ButtonPressEnded(object? sender, EventArgs e)
-    {
-        //_pwm.Duration = TimeSpan.FromMicroseconds(1500 + pwmCalibration);
-
-        //await _servo.RotateTo(new Angle(0));
-
-        _crServo?.Neutral();
-        _aServo?.Neutral();
-    }
-
-    private async void CcwButtonPressStarted(object? sender, EventArgs e)
-    {
-        //await _servo.RotateTo(new Angle(-90));
-
-        //_pwm.Duration = TimeSpan.FromMicroseconds(900 + pwmCalibration);
-        _crServo?.Rotate(RotationDirection.CounterClockwise, 1);
-        _aServo?.RotateTo(_aServo.MinimumAngle);
-    }
-
-    private async void CwButtonPressStarted(object? sender, EventArgs e)
-    {
-        if (_speed < 1) _speed += 0.1;
-        if (_speed > 1) _speed = 1;
-
-        //await _servo.RotateTo(new Angle(90));
-        //_pwm.Duration = TimeSpan.FromMicroseconds(2100 + pwmCalibration);
-        _crServo?.Rotate(RotationDirection.Clockwise, 1);
-        _aServo?.RotateTo(_aServo.MaximumAngle);
+        await _displayService.Start();
     }
 
     public static async Task Main(string[] args)
