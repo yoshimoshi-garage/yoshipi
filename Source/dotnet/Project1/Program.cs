ffd617b39ba3824b6d4d2a87c3984df06b24286b
Jorge Ramirez <ramirez.jorgea@outlook.com>
2024-08-16 21:58:42 -0700

    Add repro project to fix a color inversion bug(?)

---
+++ Source/dotnet/Project1/Program.cs
@@ -0,0 +1,13 @@
+﻿using Meadow;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await MeadowOS.Start(args);
+        }
+    }
+}
