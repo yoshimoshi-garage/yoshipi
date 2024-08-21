7788ab324e5d53d983c49ecccf22ea0f2f284f26
Chris Tacke <ctacke@gmail.com>
2024-08-21 16:54:23 -0500

    obd work

--- Source/dotnet/Samples/OBD2_ECU/MainController.cs
+++ Source/dotnet/Samples/OBD2_ECU/MainController.cs
@@ -1,5 +1,4 @@
-﻿using Meadow;
-using Meadow.Foundation.Telematics.OBD2;
+﻿using Meadow.Foundation.Telematics.OBD2;
 using Meadow.Hardware;
 using Meadow.Peripherals.Displays;
 
@@ -26,35 +25,5 @@ public class MainController
     public async Task Run()
     {
         await Display.Start();
-    }
-
-
-    private void OnFrameReceived(object? sender, ICanFrame frame)
-    {
-        if (frame is DataFrame df)
-        {
-            if (df is RemoteTransferRequestFrame rtr)
-            {
-                Resolver.Log.Info($"RTR: 0x{df.ID:X2}  {BitConverter.ToString(rtr.Payload)}");
-
-                // create a response frame
-                var response = new StandardDataFrame
-                {
-                    ID = rtr.ID,
-                    Payload = new byte[] { 0x00, 0x00, 0xde, 0xad, 0xbe, 0xef, 0x00, 0x00 }
-                };
-                CanBus.WriteFrame(response);
-            }
-            else if (df is StandardDataFrame sdf)
-            {
-                Resolver.Log.Info($"SDF: 0x{df.ID:X2}  {BitConverter.ToString(sdf.Payload)}");
-            }
-            else if (df is ExtendedDataFrame edf)
-            {
-                Resolver.Log.Info($"EDF: 0x{df.ID:X2}  {BitConverter.ToString(edf.Payload)}");
-            }
-
-            Display.DisplayReceivedFrame(df);
-        }
     }
 }
